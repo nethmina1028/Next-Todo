9cf8a5c2e9d17fa189f9605c1398d74c137550e2
Wickramasinghe W A N P <139307869+nethmina1028@users.noreply.github.com>
2024-09-08 19:48:28 +0530

    setup

---
+++ todo-app/app/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function Home() {
+  return (
+    <main className="max-w-4xl mx-auto mt-4">
+
+      <div className="text-center my-5 flex flex-col gap-4">
+        <h1 className="text-2xl font-bold">Todo List App</h1>
+        
+      </div>
+
+    </main>
+  );
+}
