a8e275764a1918b91b3e4d50e6d112794810d829
Wickramasinghe W A N P <139307869+nethmina1028@users.noreply.github.com>
2024-09-08 22:19:26 +0530

    tasks

--- todo-app/app/components/AddTask.tsx
+++ todo-app/app/components/AddTask.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { AiOutlinePlus } from 'react-icons/ai'
 
 const AddTask = () => {
   return (
-    <div>AddTask</div>
+    <div>
+        <button className='btn btn-primary w-full'>Add new task
+        <AiOutlinePlus className='ml-2' size={18}/>
+        </button>
+    </div>
   )
 }
 
