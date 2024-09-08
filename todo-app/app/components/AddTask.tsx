c3536b1c083f86f94a7fc494ab82d9f347284601
Wickramasinghe W A N P <139307869+nethmina1028@users.noreply.github.com>
2024-09-08 23:55:02 +0530

    modal and add task

--- todo-app/app/components/AddTask.tsx
+++ todo-app/app/components/AddTask.tsx
@@ -1,12 +1,50 @@
-import React from 'react'
+"use client";
+import React, { FormEventHandler, useState } from 'react'
 import { AiOutlinePlus } from 'react-icons/ai'
+import Modal from './Modal'
 
 const AddTask = () => {
+
+    const [modalOpen,setModalOpen] = useState<boolean>(false);
+    const [newTaskValue,setNewTaskValue] = useState<string>('');
+
+    const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = (e) => {
+        e.preventDefault();
+        console.log(newTaskValue);
+        setNewTaskValue('');
+    }
+
+
   return (
     <div>
-        <button className='btn btn-primary w-full'>Add new task
+        <button 
+        onClick={() =>setModalOpen(true)} 
+        className='btn btn-primary w-full'>Add new task
         <AiOutlinePlus className='ml-2' size={18}/>
+        
         </button>
+         
+        <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
+
+          <form  onSubmit={handleSubmitNewTodo} >
+            <h3 className='font-bold text-lg'>Add new task</h3>
+            <div className='modal-action '>
+              
+                <input 
+                 value={newTaskValue}
+                 onChange={e => setNewTaskValue(e.target.value)}
+                  type='text'
+                  placeholder='Task name'
+                  className='input input-bordered w-full '
+                />
+                
+                <button type='submit' className='btn'>Submit</button>
+
+            </div>
+          </form>
+
+        </Modal>
+       
     </div>
   )
 }
---
+++ todo-app/app/components/Modal.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+interface ModalProps {
+    modalOpen: boolean;
+    setModalOpen : (open : boolean) => boolean | void;
+    children: React.ReactNode
+}
+
+const Modal:React.FC<ModalProps> = ({modalOpen,setModalOpen,children}) => {
+
+  return (
+    <dialog id="my_modal_3" className={`modal ${modalOpen ? "modal-open" :""}`}>
+      <div className="modal-box">
+        <form method="dialog">
+          {/* if there is a button in form, it will close the modal */}
+          <button onClick={() =>setModalOpen(false)} className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+            ✕
+          </button>
+        </form>
+        
+        {children}
+
+
+      </div>
+    </dialog>
+  );
+};
+
+export default Modal;
